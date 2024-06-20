acf4eba1d723c753df06faabfe1b0670d3460cd4
SnezhanaJ <snezhanajankulova@gmail.com>
2024-06-20 06:16:46 +0200

    Add project files.

---
+++ BookStore.Web/Controllers/BooksController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Repository;
+using BookStore.Domain.Domain;
+
+namespace BookStore.Web.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly Repository.ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Books.Include(b => b.Author).Include(b => b.Publisher);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var books = await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.Publisher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+
+            return View(books);
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            // Fetch the authors from the database
+            var authors = _context.Authors.Select(a => new {
+                Id = a.Id,
+                FullName = a.FirstName + " " + a.LastName  // Combine first and last names
+            }).ToList();
+
+            // Create a SelectList with FullName as the text and Id as the value
+            ViewData["AuthorId"] = new SelectList(authors, "Id", "FullName");
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Name");
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Price,ReleaseDate,PublisherId,AuthorId")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                books.Id = Guid.NewGuid();
+                _context.Add(books);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id", books.AuthorId);
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Name", books.PublisherId);
+            return View(books);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var books = await _context.Books.FindAsync(id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+            var authors = _context.Authors.Select(a => new {
+                Id = a.Id,
+                FullName = a.FirstName + " " + a.LastName  // Combine first and last names
+            }).ToList();
+
+            // Create a SelectList with FullName as the text and Id as the value
+            ViewData["AuthorId"] = new SelectList(authors, "Id", "FullName");
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Name", books.PublisherId);
+            return View(books);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Title,BookImage,Price,ReleaseDate,PublisherId,AuthorId")] Books books)
+        {
+            if (id != books.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(books);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BooksExists(books.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "Id", books.AuthorId);
+
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Name", books.PublisherId);
+            return View(books);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var books = await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.Publisher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+
+            return View(books);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var books = await _context.Books.FindAsync(id);
+            if (books != null)
+            {
+                _context.Books.Remove(books);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BooksExists(Guid id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BookStore.Web/Program.cs
@@ -0,0 +1,56 @@
+using BookStore.Domain.Identity;
+using BookStore.Repository;
+using BookStore.Repository.Implementation;
+using BookStore.Repository.Interface;
+using BookStore.Service.Implementation;
+using BookStore.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddDefaultIdentity<BookStoreUsers>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        builder.Services.AddTransient<IPublisherService, PublisherService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
